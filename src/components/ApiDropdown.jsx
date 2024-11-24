9f2d67f759aeb8bfdcb97303578cc032e2cda070
mayk mendoza <miky14n@gmail.com>
2024-11-24 19:33:37 -0400

    Correccion dropdown

--- src/components/ApiDropdown.jsx
+++ src/components/ApiDropdown.jsx
@@ -7,12 +7,11 @@ import {
   Button,
 } from "@nextui-org/react";
 import { useState, useEffect } from "react";
-
 function getMenuItem(menuItems, buttonLabel, nameOfGet, id) {
   const foundItem = menuItems.find((item) => item[id] === buttonLabel);
+  console.log(" lo que retorna el metodo:", menuItems);
   return foundItem ? foundItem[nameOfGet] : null;
 }
-
 export default function ApiDropdown({
   buttonLabel,
   urlApi,
@@ -25,15 +24,18 @@ export default function ApiDropdown({
   defaultText = "Choose any option",
 }) {
   const [menuItems, setMenuItems] = useState([]);
-  const [selectedItemName, setSelectedItemName] = useState(defaultText); // Default text as initial value
+  /*const [selectedItemName, setSelectedItemName] = useState(
+    buttonLabel || "Choose an option"
+  );*/
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   useEffect(() => {
     const fetchApi = async () => {
       setLoading(true);
       try {
-        const response = await fetch(urlApi, { method: "GET" });
+        const response = await fetch(urlApi, {
+          method: "GET",
+        });
         if (!response.ok) {
           throw new Error("Error fetching data");
         }
@@ -50,24 +52,12 @@ export default function ApiDropdown({
     fetchApi();
   }, [urlApi]);
 
-  // UseEffect to update the selected item only when menuItems or buttonLabel changes
-  useEffect(() => {
-    if (menuItems.length > 0 && buttonLabel) {
-      const selectedName = getMenuItem(
-        menuItems,
-        buttonLabel,
-        nameOfGet,
-        idOfGet
-      );
-      setSelectedItemName(selectedName || defaultText); // Default text if not found
-    }
-  }, [menuItems, buttonLabel, nameOfGet, idOfGet, defaultText]);
-
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   const handleSelect = (id, name) => {
-    setSelectedItemName(name);
+    //setSelectedItemName(name);
+    console.log(id);
     onActionName(name);
     onActionId(id);
   };
@@ -75,7 +65,11 @@ export default function ApiDropdown({
   return (
     <Dropdown backdrop="blur">
       <DropdownTrigger>
-        <Button variant="bordered">{selectedItemName}</Button>
+        <Button variant="bordered">
+          {buttonLabel
+            ? getMenuItem(menuItems, buttonLabel, nameOfGet, idOfGet)
+            : defaultText}
+        </Button>
       </DropdownTrigger>
       <DropdownMenu variant="faded" aria-label="Dropdown Actions">
         {menuItems.map(
